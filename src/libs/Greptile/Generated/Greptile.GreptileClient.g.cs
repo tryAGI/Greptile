5c2da9fe5f41de713fdd8026f4342927f0a2ba6c
Konstantin S. <havendv@gmail.com>
2026-05-05 17:17:24 +0400

    feat: Updated OpenAPI spec (#16)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Greptile/Generated/Greptile.GreptileClient.g.cs
+++ src/libs/Greptile/Generated/Greptile.GreptileClient.g.cs
@@ -73,10 +73,10 @@ namespace Greptile
         /// <param name="options">Client-wide request defaults such as headers, query parameters, retries, and timeout.</param>
         /// <param name="disposeHttpClient">Dispose the HttpClient when the instance is disposed. True by default.</param>
         public GreptileClient(
-            global::System.Net.Http.HttpClient? httpClient = null,
-            global::System.Uri? baseUri = null,
-            global::System.Collections.Generic.List<global::Greptile.EndPointAuthorization>? authorizations = null,
-            global::Greptile.AutoSDKClientOptions? options = null,
+            global::System.Net.Http.HttpClient? httpClient,
+            global::System.Uri? baseUri,
+            global::System.Collections.Generic.List<global::Greptile.EndPointAuthorization>? authorizations,
+            global::Greptile.AutoSDKClientOptions? options,
             bool disposeHttpClient = true)
         {
 
